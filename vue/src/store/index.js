72931dd082a6b794616df2de3b3b41f68014efa5
medaittaleb <yamixlive@gmail.com>
2022-02-02 22:51:37 +0100

    setup vue tailwind

---
+++ vue/src/store/index.js
@@ -0,0 +1,19 @@
+import {createStore} from 'vuex'
+
+
+
+const store = createStore({
+    state: {
+        user:{
+            data:{name: "Med Talib"},
+            token: null
+        }
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {}    
+})
+
+
+export default store;
