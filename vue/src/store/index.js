092efb09f05c2a1fc31d45eaa012270a545f1058
medaittaleb <yamixlive@gmail.com>
2022-02-10 22:14:13 +0100

    setup front  register

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,17 +5,28 @@ import {createStore} from 'vuex'
 const store = createStore({
     state: {
         user:{
-            data:{
-                name: 'Med Talib',
-                email: 'tom@example.com',
-                imageUrl:
-                  'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-              },
-            token: 123
+            data:{},
+            token: null
         }
     },
     getters: {},
-    actions: {},
+    actions: {
+        register({ commit }, user){
+            return fetch(`http://localhost:8000/api/register`, {
+                headers:{
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify(user),
+            })
+            .then((res) => res.json())
+            .then((res) => {
+                commit("setUser", res);
+                return res;
+            })
+        }
+    },
     mutations: {
         logout: state => {
             state.user.data = {};
